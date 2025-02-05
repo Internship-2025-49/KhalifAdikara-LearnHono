f792612bcf511a61473cb35ea81243d9b7498726
Khahaha10 <knurfattah@gmail.com>
2025-02-05 14:31:08 +0700

    feat: add API endpoint for create posts #2 create new function createPost in controller

--- src/app/api/[...route]/routes/index.ts
+++ src/app/api/[...route]/routes/index.ts
@@ -1,11 +1,12 @@
 //import hono
 
 import { Hono } from 'hono';
-import { getPosts } from '../../../controllers/PostControllers';
+import { getPosts, createPost } from '../../../controllers/PostControllers';
 
 const app = new Hono()
 
 app.get('/', (c) => getPosts(c));
 
+app.post('/', (c) => createPost(c));
 
 export const Routes = app;
--- src/app/controllers/PostControllers.ts
+++ src/app/controllers/PostControllers.ts
@@ -3,7 +3,7 @@ import { Context } from 'hono'
 
 export const getPosts = async (c: Context) => {
     try {
-        const posts = await prisma.post.findMany({ orderBy: { id: 'desc' } });
+        const posts = await prisma.post.findMany({ orderBy: { id: 'asc' } });
 
         return c.json({
             success: true,
@@ -14,4 +14,31 @@ export const getPosts = async (c: Context) => {
     } catch (e: unknown) {
         console.error(`Error getting posts: ${e}`);
     }
+}
+
+export async function createPost(c: Context) {
+    try {
+
+    const body = await c.req.parseBody();
+
+    const title   = typeof body['title'] === 'string' ? body['title'] : '';
+    const content = typeof body['content'] === 'string' ? body['content'] : '';
+
+    const post = await prisma.post.create({
+        data: {
+            title: title,
+            content: content,
+        }
+    });
+
+    return c.json({
+        success: true,
+        message: 'Post Created Successfully!',
+        data: post
+    }, 201);
+
+    } catch (e: unknown) {
+        console.error(`Error creating post: ${e}`);
+    }
+
 }
