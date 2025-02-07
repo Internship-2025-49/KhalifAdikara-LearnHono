1d10a8b10f5539772b47bdbb3f6c5cb2c8bcfc05
Khahaha10 <knurfattah@gmail.com>
2025-02-07 08:26:31 +0700

    refactor: update API key header name #3 update error message in authentication middleware

--- src/app/middleware/Auth.ts
+++ src/app/middleware/Auth.ts
@@ -2,7 +2,7 @@ import { Context, Next } from 'hono'
 import prisma from '@/prisma/client'
 
 export const apiKeyAuth = async (c: Context, next: Next) => {
-    const apiKey = c.req.header('x-api-key')
+    const apiKey = c.req.header('api-key')
 
     if (!apiKey) {
         return c.json({ success: false, message: 'Masukan API key terlebih dahulu' }, 401)
@@ -13,7 +13,7 @@ export const apiKeyAuth = async (c: Context, next: Next) => {
     })
 
     if (!auth) {
-        return c.json({ success: false, message: 'Api key tidak ditemukan' }, 401)
+        return c.json({ success: false, message: 'Api key salah' }, 401)
     }
 
     await next()
