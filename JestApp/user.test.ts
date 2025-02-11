c00d476984c97bc07de720fe829c5de31ff59fbd
Khahaha10 <knurfattah@gmail.com>
2025-02-11 12:07:01 +0700

    test: add unit test for getUserById function #7

--- JestApp/user.test.ts
+++ JestApp/user.test.ts
@@ -1,19 +1,20 @@
 import prisma from '@/prisma/client';
-import { createUser, getUsers } from '@/src/app/controllers/UserControllers';
+import { createUser, getUserById, getUsers } from '@/src/app/controllers/UserControllers';
 import { Context } from 'hono';
+import { json } from 'stream/consumers';
 
 describe('UserController test', () => {
 
     test('getUsers test', async () => {
-        const getTest = {
+        const getUsersTest = {
             json: jest.fn(),
         } as unknown as Context;
 
         const users = await prisma.users.findMany();
 
-        await getUsers(getTest);
+        await getUsers(getUsersTest);
 
-        expect(getTest.json).toHaveBeenCalledWith(users);
+        expect(getUsersTest.json).toHaveBeenCalledWith(users);
     });
 
     test('createUser test', async () => {
@@ -23,18 +24,17 @@ describe('UserController test', () => {
                     username: 'user test',
                     name: 'nama user test',
                     address: 'alamat test',
-                    phone: '080808080808'
+                    phone: '0000000000'
                 })
             },
             json: jest.fn()
         } as unknown as Context;
 
         const newUser = {
-            id: 1,
             username: 'user test',
             name: 'nama user test',
             address: 'alamat test',
-            phone: '080808080808'
+            phone: '0000000000',
         };
 
         prisma.users.create = jest.fn().mockResolvedValue(newUser);
@@ -42,5 +42,22 @@ describe('UserController test', () => {
         await createUser(createTest);
 
         expect(createTest.json).toHaveBeenCalledWith(newUser);
+    });
+
+    test('getUserById test', async () => {
+        const getUserByIdTest = {
+            req: {
+                param: jest.fn().mockReturnValue('1')
+            },
+            json: jest.fn()
+        } as unknown as Context;
+
+        const users = await prisma.users.findUnique({
+            where: { id: 1 },
+        });
+
+        await getUserById(getUserByIdTest);
+
+        expect(getUserByIdTest.json).toHaveBeenCalledWith(users)
     });
 })
